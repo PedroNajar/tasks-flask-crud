0d09869bc0c329eb3074beccdc3baa586c5895f8
Pedro Gonzales <pedrogonzales.pn@gmail.com>
2025-03-12 16:43:30 -0300

    Add app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello world!"
+
+@app.route("/about")
+def about():
+    return "Página sobre"
+
+if __name__ == "__main__": # Usar esse código somente na fase de produção e testes.
+    app.run(debug=True)
+    
